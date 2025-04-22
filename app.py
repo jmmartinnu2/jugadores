1e00f3d027cd92379905cba169bf895e96f8dc7f
Jm <93231115+jmmartinnu2@users.noreply.github.com>
2025-04-22 13:08:29 +0200

    update

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ players = {
     "Axel Rimoldi Ayas": {
         "photo": "./img/axel-rimoldi.png",
         "crest": "./img/santafe.png",
-        "video": "https://app.veo.co/matches/20250323-jn-vs-puerto-malagueno-11b4ccf5/",
+        "video": "https://app.veo.co/matches/20250406-jn-vs-tiro-pichon-2c2dcbc3/",
         "description": (
             "**Axel Rimoldi Ayas**\n"
             "- **Edad:** 2007 (2.º año juvenil)\n"
